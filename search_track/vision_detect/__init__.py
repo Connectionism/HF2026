1cd9c76b34f0cee77b31b8fe6e4abde789e02bfa
dream <228609324@qq.com>
2026-08-11 18:49:04 +0800

    第二次修改代码

--- search_track/vision_detect/__init__.py
+++ search_track/vision_detect/__init__.py
@@ -1,11 +1,21 @@
+# src/vision_detect/__init__.py
+"""
+视觉识别模块 - 阶段二
+提供目标滤波、诱饵判别、上报优化功能
+"""
+
 from .ema_filter import EMATracker, haversine_distance
 from .decoy_classifier import DecoyClassifier
 from .report import ReportOptimizer, make_report_message, parse_report_message
+
+# 别名，兼容队长代码
+VisionDetect = DecoyClassifier
 
 __all__ = [
     'EMATracker',
     'haversine_distance',
     'DecoyClassifier',
+    'VisionDetect',
     'ReportOptimizer',
     'make_report_message',
     'parse_report_message',
